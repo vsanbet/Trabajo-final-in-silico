8c8c0acad4be1d551db1b3d05cbb703763b30d13
vsanbet <vsanbet@gmail.com>
2026-02-04 00:19:21 +0100

    Add QC check and update analysis title in ejemplo_uso.R
    Added a new run_geyer_pipeline call for QC check and updated analysis title for Platelet-rich Plasma analysis.

--- ejemplo_uso.R
+++ ejemplo_uso.R
@@ -1,6 +1,14 @@
 # ejemplo_uso.R
 source("run_geyer_pipeline.R")
 
+# Obtención de grupos
+run_geyer_pipeline(
+     path = "ruta/a/tu/dataset", 
+     analysis_title = "QC check", 
+     inspect_samples = TRUE'
+)
+
+# Análisis de ficheros
 run_geyer_pipeline(
      path = "path/to/MaxQuant_output",
      group_regex = c("Dilution", "Thrombocytes"),
